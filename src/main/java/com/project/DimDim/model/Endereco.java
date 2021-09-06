447eb1b2014eef89078dd7b186362596270991f7
Bruuno96 <brunotkd96@gmail.com>
2021-09-06 15:00:22 -0300

    index page

---
+++ src/main/java/com/project/DimDim/model/Endereco.java
@@ -0,0 +1,30 @@
+package com.project.DimDim.model;
+
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="TB_DIMDIM_ENDERECO")
+@SequenceGenerator(allocationSize = 1, name = "seq_endereco", sequenceName = "SQ_TB_DIMDIM_ENDERECO")
+public class Endereco {
+	
+	@Id
+	@Column(name="id_endereco")
+	@GeneratedValue(generator = "seq_endereco")
+	private Long idEndereco;
+	
+	@Column(name="ds_rua")
+	private String rua;
+	
+	@Column(name="nr_numero")
+	private int numero;
+	
+	@Column(name="nm_estado")
+	private String estado;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_cliente")
+	private Cliente cliente;
+	
+}
