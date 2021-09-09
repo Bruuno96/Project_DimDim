99dd799791cbd670eef84513ab15198db7195d90
murilonerdx <mu-silva@outlook.com>
2021-09-09 09:21:59 -0300

    Arrumando nomes

--- src/main/java/com/project/DimDim/services/EnderecoService.java
+++ src/main/java/com/project/DimDim/services/EnderecoService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class EndereçoRepository {
+public class EnderecoService {
 
     @Autowired
     private EnderecoRepository repository;
